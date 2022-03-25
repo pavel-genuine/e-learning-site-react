5cb0a1e1bc4d140956172c47f4befa0aff5288d8
pavel.genuine <pavel.play@gmail.com>
2022-03-26 03:20:05 +0600

    choose one

--- src/components/selectedItems/SelectedItems.js
+++ src/components/selectedItems/SelectedItems.js
@@ -1,10 +1,10 @@
-import React from 'react';
+import React, { useState } from 'react';
 
 const SelectedItems = (props) => {
 
     const {selectedItems}=props
 
-    const names =[]
+    let names =[]
     const images=[]
 
     for (const item of selectedItems){
@@ -14,13 +14,38 @@ const SelectedItems = (props) => {
         names.push(name)
         images.push(image)
     }
-    const newNames = names.map(name=><li key={name} > {name}</li>)
+    let newNames = names.map(name=><li key={name} > {name}</li>)
     const newimgs = images.map(image=>  <img style={{width:'50px'}} src={image} alt="" />)
+
+    const [choose, setChoose] = useState('')
+
+    const chooseOne = ()=>{
+
+        let i = Math.floor(Math.random() * 3)+1
+        const oneName =names[i]
+        setChoose(oneName)
+    }
+
+    const [chooseAgain, setChooseAgain] =useState([])
+
+    const againChoose = ()=>{
+        newNames.length=0
+        setChooseAgain(names)
+    }
 
     return (
         <div>
+             <div>
             Selected Items :
-        {newNames}
+            {newNames}
+        
+
+        </div>
+
+      <button onClick={chooseOne} >Choose One For Me</button>
+       <button onClick={againChoose} >Choose Again</button>
+         <h6>Choosed One : {choose}</h6>
+       
         </div>
     );
 };
